9c963be439630666f7ff5dd277cad5c0cac547a9
Soawnjaja <basstraper@gmail.com>
2023-10-31 19:32:23 +0300

    added func to find path, read and parse file. -not complete

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,13 +1,15 @@
 #!/usr/bin/env node
 import { program } from 'commander';
-
+import genDiff from './index.js';
 program
   .version('1.0.0')
   .description('Compares two configuration files and shows a difference.')
   .option('-h, --help', 'output usage information')
   .arguments('[filepath1] [filepath2]')
   .option('-f, --format <type>', 'output format' )
-  .parse(process.argv);
+  .action((filepath1, filepath2) => {
+    console.log(genDiff (filepath1, filepath2))
+  });
 
   const option = program.opts();
 if(option.help || (!program.filepath1 || !program.filepath2)) {
@@ -16,3 +18,5 @@ if(option.help || (!program.filepath1 || !program.filepath2)) {
 if(option.format) {
   console.log( 'You chose type `${type}`')
 }
+
+program.parse();
--- bin/index.js
+++ bin/index.js
@@ -1,4 +1,14 @@
 #!/usr/bin/env node
+import fs from 'fs';
+import path from 'path';
 
-// const diff = genDiff(filepath1, filepath2);
-// console.log(diff);
+const readFile = fs.readFileSync;
+const getPathFile = (pathF) =>  path.isAbsolute(pathF) ? pathF : path.resolve(process.cwd(), 'frontend-project-46', pathF);
+export default function genDiff(filepath1, filepath2) {
+    const filePath1 = getPathFile(filepath1);
+    const filePath2 = getPathFile(filepath2);
+    const file1 = readFile(filePath1);
+    const file2 = readFile(filePath2);
+
+    return [JSON.parse(file1), JSON.parse(file2)];
+}
