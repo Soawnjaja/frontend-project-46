298bf412a250d082551793afa8fa1345b9551d99
Soawnjaja <basstraper@gmail.com>
2023-11-01 02:28:40 +0300

    added function to show diff step 4

--- bin/index.js
+++ bin/index.js
@@ -14,6 +14,19 @@ import getParsedFile from './parser.js'
 export default function genDiff(filepath1, filepath2) {
     const file1 = transformPathToFileObject(filepath1);
     const file2 = transformPathToFileObject(filepath2);
-
-    return [file1, file2 ];
+    const keys1 = Object.keys(file1);
+    const keys2 = Object.keys(file2);
+    const sortedKeys1 = keys1.sort();
+    const result = sortedKeys1.map((item) => {
+      if (!keys2.includes(item)) {
+        console.log(`- ${item}: ${file1[item]}`);
+      } 
+      if(keys2.includes(item) && file1[item] === file2[item]) {
+        console.log(`  ${item}: ${file1[item]}`);
+      } 
+      if(keys2.includes(item) && file1[item] !== file2[item]) {
+        console.log(`- ${item} : ${file1[item]}`);
+        console.log(`+ ${item} : ${file2[item]}`);
+      }
+    });
 }
