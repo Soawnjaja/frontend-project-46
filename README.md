05f3f794e37093295450d6a4acd31623b1fa6734
Soawnjaja <basstraper@gmail.com>
2023-11-02 14:35:55 +0300

    added preview in README.md

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/Soawnjaja/frontend-project-46/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/Soawnjaja/frontend-project-46/actions)
+### Preview 
+[![asciicast](https://asciinema.org/a/aTHTTYN3EYIoKmgb72jblr2rl.svg)](https://asciinema.org/a/aTHTTYN3EYIoKmgb72jblr2rl)
--- bin/index.js
+++ bin/index.js
@@ -2,31 +2,36 @@
 import fs from 'fs';
 import path from 'path';
 import getParsedFile from './parser.js'
-
-
- const transformPathToFileObject = (file) => {
-    const filePath = path.isAbsolute(file) ? file : path.resolve(process.cwd(), file);
-    const read = fs.readFileSync(`${filePath}`, 'utf8');
-    const readJson = getParsedFile(read, filePath);
+import { cwd } from 'process';
+const getPath = (filepath) => path.resolve(cwd(), filepath);
+ const transformPathToFileObject = (filepath) => {
+ const fullPath = getPath(filepath);
+ const read = fs.readFileSync(`${fullPath}`, 'utf8');
+ const readJson = getParsedFile(read, fullPath);
     return readJson;
   };
 
 export default function genDiff(filepath1, filepath2) {
-    const file1 = transformPathToFileObject(filepath1);
-    const file2 = transformPathToFileObject(filepath2);
-    const keys1 = Object.keys(file1);
-    const keys2 = Object.keys(file2);
-    const sortedKeys1 = keys1.sort();
-    const result = sortedKeys1.map((item) => {
-      if (!keys2.includes(item)) {
-        console.log(`- ${item}: ${file1[item]}`);
-      } 
-      if(keys2.includes(item) && file1[item] === file2[item]) {
-        console.log(`  ${item}: ${file1[item]}`);
-      } 
-      if(keys2.includes(item) && file1[item] !== file2[item]) {
-        console.log(`- ${item} : ${file1[item]}`);
-        console.log(`+ ${item} : ${file2[item]}`);
-      }
-    });
+const file1 = transformPathToFileObject(filepath1);
+const file2 = transformPathToFileObject(filepath2);
+const keys1 = Object.keys(file1);
+const keys2 = Object.keys(file2);
+const allKeys = new Set([...keys1, ...keys2]);
+const sortedKeys = Array.from(allKeys).sort();
+const result = sortedKeys.reduce((acc, item) => {
+    if (!keys2.includes(item)) {
+      return acc + `- ${item}: ${file1[item]}\n`;
+    } 
+    if (!keys1.includes(item)) {
+      return acc + `+ ${item}: ${file2[item]}\n`;
+    }
+    if (file1[item] === file2[item]) {
+      return acc + `  ${item}: ${file1[item]}\n`;
+    } 
+    if (file1[item] !== file2[item]) {
+      return acc + `- ${item}: ${file1[item]}\n` + `+ ${item}: ${file2[item]}\n`;
+    }
+    return acc;
+  }, "{\n") + "}";
+  return result;
 }
