e3339dfa58f6b168f330993e6afc2816f0a3de3f
Soawnjaja <basstraper@gmail.com>
2023-11-13 03:07:59 +0300

    fix linter errors, fix package.json

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 #!/usr/bin/env node
-  //главный модуль
+// главный модуль
 
 import fs from 'fs';
 import path from 'path';
 import { cwd } from 'process';
 import getParsedFile from './parser.js';
-import stylish from './formatter.js';
-import buildDiffs from './buildDiffs.js';
+import getDiff from './formatter.js';
+import makeDiff from './buildDiffs.js';
 
 const getPath = (filepath) => path.resolve(cwd(), '__fixtures__', filepath);
 const transformPathToFileData = (filepath) => {
@@ -17,9 +17,15 @@ const transformPathToFileData = (filepath) => {
 };
 
 const genDiff = (file1, file2, format = 'stylish') => {
- const readData1 = transformPathToFileData(file1);
- const readData2 = transformPathToFileData(file2);
- const dataDiffs = buildDiffs(readData1,readData2);
- return stylish(dataDiffs,format);
+  if (format === 'stylish') {
+    const readData1 = transformPathToFileData(file1);
+    const readData2 = transformPathToFileData(file2);
+    const dataDiff = getDiff(readData1, readData2);
+    return makeDiff(dataDiff, format);
+  }
+  const readData1 = transformPathToFileData(file1);
+  const readData2 = transformPathToFileData(file2);
+  const dataDiff = getDiff(readData1, readData2);
+  return makeDiff(dataDiff, format);
 };
 export default genDiff;
