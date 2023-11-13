80d3d0348da59bab751c58819d19ef7dc47bc809
Soawnjaja <basstraper@gmail.com>
2023-11-13 22:22:30 +0300

    edit test file

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -12,11 +12,11 @@ const result = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
 const expectedResult = result('result_expected.txt');
 const expectedTreeResult = result('result_tree_expected.txt');
-// прочитали файлы
-// test('gendiff flat', () => {
-//   expect(genDiff('file1.json', 'file2.json')).toEqual(expectedResult);
-//   expect(genDiff('file1-yaml.yaml', 'file2-yaml.yaml')).toEqual(expectedResult);
-// });
+
+test('gendiff flat', () => {
+  expect(genDiff('file1.json', 'file2.json')).toEqual(expectedResult);
+  expect(genDiff('file1-yaml.yaml', 'file2-yaml.yaml')).toEqual(expectedResult);
+});
 test('gendiff tree', () => {
   expect(genDiff('tree1.json', 'tree2.json')).toEqual(expectedTreeResult);
   expect(genDiff('treeYAML1.yaml', 'treeYAML2.yaml')).toEqual(expectedTreeResult);
