048c8c450d8e1292eec8324cd36ed9b57ffe8ef5
Soawnjaja <basstraper@gmail.com>
2023-11-28 14:40:51 +0300

    rework logic in index.js to prepeare data for compare

--- src/index.js
+++ src/index.js
@@ -4,18 +4,17 @@ import parse from './parser.js';
 import getReport from './formatter/index.js';
 import compareData from './buildDiffs.js';
 
-const getAbsolutPath = (filepath) => path.resolve(process.cwd(), filepath);
-const readFile = (filepath) => fs.readFileSync(getAbsolutPath(filepath), 'utf-8');
 const getFormat = (filepath) => path.extname(filepath).slice(1);
-const genDiff = (file1, file2, formatName = 'stylish') => {
-  const value1 = readFile(file1);
-  const value2 = readFile(file2);
-  const formatFile1 = getFormat(file1);
-  const formatFile2 = getFormat(file2);
-  const data1 = parse(value1, formatFile1);
-  const data2 = parse(value2, formatFile2);
-  const tree = compareData(data1, data2);
 
+const readFile = (filepath) => {
+  const pathFull = path.resolve(process.cwd(), filepath);
+  return parse(fs.readFileSync(pathFull, 'utf-8'), getFormat(filepath));
+};
+
+const genDiff = (filepath1, filepath2, formatName = 'stylish') => {
+  const data1 = readFile(filepath1);
+  const data2 = readFile(filepath2);
+  const tree = compareData(data1, data2);
   return getReport(tree, formatName);
 };
 export default genDiff;
