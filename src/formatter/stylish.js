fcb0f4e50b064c8889f7d18e3960b024bb730649
Soawnjaja <basstraper@gmail.com>
2023-12-10 22:29:05 +0300

    3rd stage of refactoring complete

--- src/formatter/stylish.js
+++ src/formatter/stylish.js
@@ -9,10 +9,10 @@ const stringify = (data, depth) => {
   if (!_.isObject(data)) {
     return String(data);
   }
-  const arr = Object.entries(data);
-  const lines = arr.map(([key, val]) => `${setIndention(depth + 1)}  ${key}: ${stringify(val, depth + 1)}`);
+  const lines = Object.entries(data);
+  const line = lines.map(([key, val]) => `${setIndention(depth + 1)}  ${key}: ${stringify(val, depth + 1)}`);
   const result = `{
-${lines.join('\n')}
+${line.join('\n')}
  ${setIndention(depth)} }`;
   return result;
 };
